e460e671a6e02dd5e33802b5b8f5e496dcfb247a
samvelie <sam.velie@gmail.com>
2017-04-27 22:00:00 -0500

    1st user input

--- Program.cs
+++ Program.cs
@@ -5,8 +5,14 @@ namespace hwapp
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            string name;
+
+            Console.WriteLine("Enter your name please.");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
         }
     }
 }
